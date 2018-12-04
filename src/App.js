2e289dcfda2e3aabb01d95117308d927329e81cc
Lee Len Formento <leebatef@gmail.com>
2018-12-04 12:37:46 -0500

    napanood mula sa leksyon

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    isOn: false,
+  };
+
   render() {
+    const displayText = this.state.isOn ? 'On' : 'Off';
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1 className="display">{displayText}</h1>
+        <div>
+          <button onClick={this.toggle} className="toggle-btn">
+            Toggle
+          </button>
+        </div>
       </div>
     );
   }
+
+  toggle = () => {
+    this.setState(prevState => ({ isOn: !prevState.isOn }));
+  };
 }
 
 export default App;
